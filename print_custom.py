7c77107674da914fbb30938b0a0ec72bace4a0e6
richard <hanhu@mie.utoronto.ca>
2020-03-25 17:34:46 -0400

    added debugging decorators

--- print_custom.py
+++ print_custom.py
@@ -259,3 +259,28 @@ def mem_report():
     _mem_report(cuda_tensors, 'GPU')
     _mem_report(host_tensors, 'CPU')
     print('=' * LEN)
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.time()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.time()      # 2
+        run_time = end_time - start_time    # 3
+        db.printColour("Finished {} in {:.4f} secs".format(func.__name__, run_time))
+        return value
+    return wrapper_timer
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]                      # 1
+        kwargs_repr = ["{}={}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        db.printColour("Calling {}({})".format(func.__name__, signature))
+        value = func(*args, **kwargs)
+        db.printColour("{} returned {}".format(func.__name__, value))           # 4
+        return value
+    return wrapper_debug
